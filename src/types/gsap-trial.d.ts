3efd1c65e692b6e998fabf17fa03e97379a3a749
Mayank Mishra <mayank.mishra@wdimails.com>
2026-05-22 12:26:40 +0530

    fixed the issue

---
+++ src/types/gsap-trial.d.ts
@@ -0,0 +1,10 @@
+// src/types/gsap-trial.d.ts
+
+declare module "gsap-trial/SplitText" {
+  const SplitText: any;
+  export default SplitText;
+}
+
+declare module "gsap-trial/ScrollSmoother" {
+  export const ScrollSmoother: any;
+}
